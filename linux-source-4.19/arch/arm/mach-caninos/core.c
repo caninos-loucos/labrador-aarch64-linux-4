889c680c363e13b1036029be7432b1cd523d181d
edgarlsitec <edgar.righi@lsitec.org.br>
2023-02-10 19:10:12 -0300

    The 32-bit core board initialization driver is complete.

--- linux-source-4.19/arch/arm/mach-caninos/core.c
+++ linux-source-4.19/arch/arm/mach-caninos/core.c
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Support Routines for the Caninos Labrador 32bits Architecture
+ *
+ * Copyright (c) 2023 ITEX - LSITEC - Caninos Loucos
+ * Author: Edgar Bernardi Righi <edgar.righi@lsitec.org.br>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <linux/irqchip.h>
 #include <linux/of_platform.h>
 #include <linux/memblock.h>
@@ -5,16 +22,44 @@
 #include <../mm/mm.h>
 #include <mach/platform.h>
 
+/*
+ CPU Recommended Frequency/Voltage Pairs
+
+  408MHz at  950mV
+  720MHz at  975mV
+  900MHz at 1025mV
+ 1104MHz at 1175mV
+ 1308MHz at 1250mV
+
+ PMIC DCDC1 Regulator Operating Limits
+
+ Max Voltage : 1400mV
+ Min Voltage :  700mV
+ Voltage Step:   25mV
+ Min Selector:    0
+ Max Selector:   28 
+ Stable After:  350us
+ 
+*/
+
+#define CPU_CORE_FREQ (1104U) /* MHz */
+#define CPU_CORE_VOLT (1175U) /* mV  */
+
+#define KINFO_SIZE            (0x00100000)
+#define FRAME_BUFFER_SIZE     (0x00800000)
+#define DDR_DQS_TRAINING_BASE (0x00000000)
+#define DDR_DQS_TRAINING_SIZE (0x00004000)
+
 static struct map_desc caninos_k5_io_desc[] __initdata = {
 	{
-		.virtual = IO_ADDRESS(K5_PA_REG_BASE),
-		.pfn     = __phys_to_pfn(K5_PA_REG_BASE),
-		.length  = K5_PA_REG_SIZE,
+		.virtual = IO_ADDRESS(PA_REG_BASE),
+		.pfn     = __phys_to_pfn(PA_REG_BASE),
+		.length  = PA_REG_SIZE,
 		.type    = MT_DEVICE,
 	},
 	{
-		.virtual = K5_VA_BOOT_RAM,
-		.pfn     = __phys_to_pfn(K5_PA_BOOT_RAM),
+		.virtual = VA_BOOT_RAM,
+		.pfn     = __phys_to_pfn(PA_BOOT_RAM),
 		.length  = SZ_4K,
 		.type    = MT_MEMORY_RWX,
 	},
@@ -37,13 +82,7 @@ static struct of_device_id caninos_k5_dt_match_table[] __initdata = {
 
 void __init caninos_k5_init(void)
 {
-	int ret;
-	
-	//pm_power_off = board_pm_halt;
-	
-	ret = of_platform_populate(NULL, caninos_k5_dt_match_table, NULL, NULL);
-	
-	if (ret) {
+	if (of_platform_populate(NULL, caninos_k5_dt_match_table, NULL, NULL)) {
 		panic("of_platform_populate() failed\n");
 	}
 }
@@ -52,11 +91,21 @@ void __init caninos_k5_reserve(void)
 {
 	phys_addr_t kinfo_start, framebuffer_start;
 	
-	framebuffer_start = arm_lowmem_limit - K5_FRAME_BUFFER_SIZE;
-	kinfo_start = framebuffer_start - K5_KINFO_SIZE;
+	framebuffer_start = arm_lowmem_limit - FRAME_BUFFER_SIZE;
+	kinfo_start = framebuffer_start - KINFO_SIZE;
 	
-	memblock_reserve(0x0, K5_DDR_DQS_TRAINING_SIZE);
-	memblock_reserve(framebuffer_start, K5_FRAME_BUFFER_SIZE);
-	memblock_reserve(kinfo_start, K5_KINFO_SIZE);
+	memblock_reserve(DDR_DQS_TRAINING_BASE, DDR_DQS_TRAINING_SIZE);
+	memblock_reserve(framebuffer_start, FRAME_BUFFER_SIZE);
+	memblock_reserve(kinfo_start, KINFO_SIZE);
+}
+
+void __init caninos_k5_init_early(void)
+{
+	if (!caninos_k5_pmic_setup()) {
+		panic("Could not setup the PMIC\n");
+	}
+	else if (!caninos_k5_cpu_set_clock(CPU_CORE_FREQ, CPU_CORE_VOLT)) {
+		panic("Could not set CPU core speed\n");
+	}
 }
 
--- linux-source-4.19/arch/arm/mach-caninos/smp.c
+++ linux-source-4.19/arch/arm/mach-caninos/smp.c
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Support Routines for the Caninos Labrador 32bits Architecture
+ *
+ * Copyright (c) 2023 ITEX - LSITEC - Caninos Loucos
+ * Author: Edgar Bernardi Righi <edgar.righi@lsitec.org.br>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <linux/delay.h>
 #include <asm/smp.h>
 #include <asm/smp_plat.h>
@@ -5,13 +22,27 @@
 #include <asm/cacheflush.h>
 #include <mach/platform.h>
 
+#define BOOT_FLAG (0x55AA)
+
+#define SPS_PG_CTL_CPU2_PWR_BIT  (5U)
+#define SPS_PG_CTL_CPU2_ACK_BIT  (21U)
+#define SPS_PG_CTL_CPU3_PWR_BIT  (6U)
+#define SPS_PG_CTL_CPU3_ACK_BIT  (22U)
+
+#define CMU_CORECTL_CPU2_RST_BIT (6U)
+#define CMU_CORECTL_CPU2_PWR_BIT (2U)
+#define CMU_CORECTL_CPU3_RST_BIT (7U)
+#define CMU_CORECTL_CPU3_PWR_BIT (3U)
+
+#define CMU_DEVRST1_DBG1_BIT     (29U)
+
 static DEFINE_RAW_SPINLOCK(boot_lock);
 
 static void __init caninos_k5_smp_init_cpus(void)
 {
 	unsigned int i, ncores;
 	
-	ncores = scu_get_core_count((void __iomem *)IO_ADDRESS(K5_PA_SCU));
+	ncores = scu_get_core_count((void __iomem *)IO_ADDRESS(PA_SCU));
 	
 	if (ncores > nr_cpu_ids) {
 		ncores = nr_cpu_ids;
@@ -23,7 +54,7 @@ static void __init caninos_k5_smp_init_cpus(void)
 
 static void __init caninos_k5_smp_prepare_cpus(unsigned int max_cpus)
 {
-	scu_enable((void __iomem *)IO_ADDRESS(K5_PA_SCU));
+	scu_enable((void __iomem *)IO_ADDRESS(PA_SCU));
 }
 
 static void __init write_pen_release(int val)
@@ -44,7 +75,7 @@ static void __init caninos_k5_smp_secondary_init(unsigned int cpu)
 
 static inline bool __init sps_power_check(u32 ackbit)
 {
-	return !!(io_readl(K5_SPS_PG_CTL) & BIT(ackbit));
+	return !!(io_readl(SPS_PG_CTL) & BIT(ackbit));
 }
 
 static inline bool __init sps_power_on(u32 pwrbit, u32 ackbit)
@@ -56,7 +87,7 @@ static inline bool __init sps_power_on(u32 pwrbit, u32 ackbit)
 		return true;
 	}
 	
-	io_writel(io_readl(K5_SPS_PG_CTL) | BIT(pwrbit), K5_SPS_PG_CTL);
+	io_writel(io_readl(SPS_PG_CTL) | BIT(pwrbit), SPS_PG_CTL);
 	
 	while (timeout > 0 && !sps_power_check(ackbit)) {
 		udelay(timestep);
@@ -77,31 +108,30 @@ static inline bool __init cpu_power_on(unsigned int cpu)
 	switch (cpu)
 	{
 	case 1:
-		clk_rstbit = K5_DBG1_CLOCK_RST_BIT;
+		clk_rstbit = CMU_DEVRST1_DBG1_BIT;
 		
 		/* assert DBG1 reset */
-		io_writel(io_readl(K5_CMU_DEVRST1) & ~BIT(clk_rstbit), K5_CMU_CORECTL);
-		
+		io_writel(io_readl(CMU_DEVRST1) & ~BIT(clk_rstbit), CMU_DEVRST1);
 		udelay(10);
 		
 		/* deassert DBG1 reset */
-		io_writel(io_readl(K5_CMU_DEVRST1) | BIT(clk_rstbit), K5_CMU_CORECTL);
+		io_writel(io_readl(CMU_DEVRST1) | BIT(clk_rstbit), CMU_DEVRST1);
 		
 		udelay(10);
 		return true;
 	
 	case 2:
-		sps_pwrbit = K5_CPU2_POWER_PWR_BIT;
-		sps_ackbit = K5_CPU2_POWER_ACK_BIT;
-		clk_rstbit = K5_CPU2_CLOCK_RST_BIT;
-		clk_pwrbit = K5_CPU2_CLOCK_PWR_BIT;
+		sps_pwrbit = SPS_PG_CTL_CPU2_PWR_BIT;
+		sps_ackbit = SPS_PG_CTL_CPU2_ACK_BIT;
+		clk_rstbit = CMU_CORECTL_CPU2_RST_BIT;
+		clk_pwrbit = CMU_CORECTL_CPU2_PWR_BIT;
 		break;
 		
 	case 3:
-		sps_pwrbit = K5_CPU3_POWER_PWR_BIT;
-		sps_ackbit = K5_CPU3_POWER_ACK_BIT;
-		clk_rstbit = K5_CPU3_CLOCK_RST_BIT;
-		clk_pwrbit = K5_CPU3_CLOCK_PWR_BIT;
+		sps_pwrbit = SPS_PG_CTL_CPU3_PWR_BIT;
+		sps_ackbit = SPS_PG_CTL_CPU3_ACK_BIT;
+		clk_rstbit = CMU_CORECTL_CPU3_RST_BIT;
+		clk_pwrbit = CMU_CORECTL_CPU3_PWR_BIT;
 		break;
 		
 	default:
@@ -109,7 +139,7 @@ static inline bool __init cpu_power_on(unsigned int cpu)
 	}
 	
 	/* assert cpu core reset */
-	io_writel(io_readl(K5_CMU_CORECTL) & ~BIT(clk_rstbit), K5_CMU_CORECTL);
+	io_writel(io_readl(CMU_CORECTL) & ~BIT(clk_rstbit), CMU_CORECTL);
 	
 	/* try to power on */
 	if (!sps_power_on(sps_pwrbit, sps_ackbit)) {
@@ -117,10 +147,10 @@ static inline bool __init cpu_power_on(unsigned int cpu)
 	}
 	
 	/* enable cpu core clock */
-	io_writel(io_readl(K5_CMU_CORECTL) | BIT(clk_pwrbit), K5_CMU_CORECTL);
+	io_writel(io_readl(CMU_CORECTL) | BIT(clk_pwrbit), CMU_CORECTL);
 	
 	/* deassert cpu core reset */
-	io_writel(io_readl(K5_CMU_CORECTL) | BIT(clk_rstbit), K5_CMU_CORECTL);
+	io_writel(io_readl(CMU_CORECTL) | BIT(clk_rstbit), CMU_CORECTL);
 	
 	udelay(10);
 	return true;
@@ -131,25 +161,27 @@ static int __init caninos_k5_smp_boot_secondary(unsigned int cpu,
 {
 	unsigned long timeout;
 	
-	if (!cpu_power_on(cpu)) {
+	if (!cpu_power_on(cpu))
+	{
+		pr_err("Could not power on cpu%u\n", cpu);
 		return -ENOSYS;
 	}
 	
 	switch (cpu)
 	{
 	case 1:
-		io_writel(virt_to_phys(caninos_k5_secondary_startup), K5_CPU1_ADDR);
-		io_writel(K5_BOOT_FLAG, K5_CPU1_FLAG);
+		io_writel(virt_to_phys(caninos_k5_secondary_startup), CPU1_ADDR);
+		io_writel(BOOT_FLAG, CPU1_FLAG);
 		break;
 		
 	case 2:
-		io_writel(virt_to_phys(caninos_k5_secondary_startup), K5_CPU2_ADDR);
-		io_writel(K5_BOOT_FLAG, K5_CPU2_FLAG);
+		io_writel(virt_to_phys(caninos_k5_secondary_startup), CPU2_ADDR);
+		io_writel(BOOT_FLAG, CPU2_FLAG);
 		break;
 	
 	case 3:
-		io_writel(virt_to_phys(caninos_k5_secondary_startup), K5_CPU3_ADDR);
-		io_writel(K5_BOOT_FLAG, K5_CPU3_FLAG);
+		io_writel(virt_to_phys(caninos_k5_secondary_startup), CPU3_ADDR);
+		io_writel(BOOT_FLAG, CPU3_FLAG);
 		break;
 	}
 	
@@ -171,24 +203,29 @@ static int __init caninos_k5_smp_boot_secondary(unsigned int cpu,
 	switch (cpu)
 	{
 	case 1:
-		io_writel(0U, K5_CPU1_ADDR);
-		io_writel(0U, K5_CPU1_FLAG);
+		io_writel(0U, CPU1_ADDR);
+		io_writel(0U, CPU1_FLAG);
 		break;
 		
 	case 2:
-		io_writel(0U, K5_CPU2_ADDR);
-		io_writel(0U, K5_CPU2_FLAG);
+		io_writel(0U, CPU2_ADDR);
+		io_writel(0U, CPU2_FLAG);
 		break;
 		
 	case 3:
-		io_writel(0U, K5_CPU3_ADDR);
-		io_writel(0U, K5_CPU3_FLAG);
+		io_writel(0U, CPU3_ADDR);
+		io_writel(0U, CPU3_FLAG);
 		break;
 	}
 	
 	raw_spin_unlock(&boot_lock);
 	
-	return pen_release != -1 ? -ENOSYS : 0;
+	if (pen_release != -1)
+	{
+		pr_err("Could not run secondary boot code at cpu%u\n", cpu);
+		return -ENOSYS;
+	}
+	return 0;
 }
 
 static struct smp_operations caninos_k5_smp_ops __initdata =
