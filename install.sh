bc03777e7fe87821793348d187decd2e19ff5c26
edgarlsitec <edgar.righi@lsitec.org.br>
2023-09-25 15:41:09 -0300

    Improved install scripts.

--- install.sh
+++ install.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 USER=$(whoami)
-sudo rm -f /media/${USER}/linux64/boot/*.dtb
-sudo rm -rf /media/${USER}/linux64/lib/modules
-sudo cp output/Image /media/${USER}/linux64/boot
-sudo cp output/*.dtb /media/${USER}/linux64/boot
-sudo cp -r output/lib/modules /media/${USER}/linux64/lib/
+
+if [ -d /media/linux64 ]; then
+	echo "linux64 mounted in /media"
+	BOOTDIR=/media/linux64/boot/
+	LIBDIR=/media/linux64/lib/
+elif [ -d /media/${USER}/linux64 ]; then
+	echo "linux64 mounted in /media/user"
+	BOOTDIR=/media/${USER}/linux64/boot/
+	LIBDIR=/media/${USER}/linux64/lib/
+elif [ -d /run/media/${USER}/linux64 ]; then
+	echo "linux64 mounted in /run/media/user"
+	BOOTDIR=/run/media/${USER}/linux64/boot/
+	LIBDIR=/run/media/${USER}/linux64/lib/
+else
+	echo "linux64 directory not found or mounted, quitting."
+	exit
+fi 
+
+
+if [ -d "$BOOTDIR" ]; then
+	sudo rm -f $BOOTDIR/*.dtb
+	sudo rm -f $BOOTDIR/Image
+else
+	sudo mkdir $BOOTDIR
+fi
+
+if [ -d "$LIBDIR" ]; then
+	sudo rm -rf $LIBDIR/modules
+fi
+
+sudo cp output/Image $BOOTDIR
+sudo cp output/*.dtb $BOOTDIR
+sudo cp -r output/lib/modules $LIBDIR
 sync
--- install32.sh
+++ install32.sh
@@ -1,8 +1,46 @@
 #!/bin/sh
 USER=$(whoami)
-sudo rm -f /media/${USER}/BOOT/*.dtb
-sudo rm -rf /media/${USER}/SYSTEM/lib/modules
-sudo cp output32/uImage /media/${USER}/BOOT/
-sudo cp output32/*.dtb /media/${USER}/BOOT/
-sudo cp -r output32/lib/modules /media/${USER}/SYSTEM/lib/
+
+if [ -d /media/BOOT ]; then
+	echo "BOOT mounted in /media"
+	BOOTDIR=/media/BOOT/
+elif [ -d /media/${USER}/BOOT ]; then
+	echo "BOOT mounted in /media/user"
+	BOOTDIR=/media/${USER}/BOOT/
+elif [ -d /run/media/${USER}/BOOT ]; then
+	echo "BOOT mounted in /run/media/user"
+	BOOTDIR=/run/media/${USER}/BOOT/
+else
+	echo "BOOT directory not found or mounted, quitting."
+	exit
+fi 
+
+if [ -d /media/SYSTEM ]; then
+	echo "SYSTEM mounted in /media"
+	LIBDIR=/media/SYSTEM/lib/
+elif [ -d /media/${USER}/SYSTEM ]; then
+	echo "SYSTEM mounted in /media/user"
+	LIBDIR=/media/${USER}/SYSTEM/lib/
+elif [ -d /run/media/${USER}/SYSTEM ]; then
+	echo "SYSTEM mounted in /run/media/user"
+	LIBDIR=/run/media/${USER}/SYSTEM/lib/
+else
+	echo "SYSTEM directory not found or mounted, quitting."
+	exit
+fi 
+
+if [ -d "$BOOTDIR" ]; then
+	sudo rm -f $BOOTDIR/*.dtb
+	sudo rm -f $BOOTDIR/Image
+else
+	sudo mkdir $BOOTDIR
+fi
+
+if [ -d "$LIBDIR" ]; then
+	sudo rm -rf $LIBDIR/modules
+fi
+
+sudo cp output/Image $BOOTDIR
+sudo cp output/*.dtb $BOOTDIR
+sudo cp -r output/lib/modules $LIBDIR
 sync
